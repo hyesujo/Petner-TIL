ed40a8fa2d8eaffa426134b064e33dd3d2e0c640
hyesujo <63487097+hyesujo@users.noreply.github.com>
2021-05-21 19:33:10 +0900

    Create 210521.m

---
+++ 14Week/210521.m
@@ -0,0 +1,15 @@
+210521
+===============
+<b>dart</b>
+
+iterable 의 정의 
+- iterable : 한번에 하나의 member를 반환할 수 있는 object(객체)를 의미 iterator로 변환이 가능
+
+``` dart
+ .any((e) => true)
+```
+- 모든 요소를 체크하고 하나라도 맞으면 true를 반환함
+``` dart
+ .every()
+```
+- 모든 요소를 체크하고 다 맞으면 true를 반환함
